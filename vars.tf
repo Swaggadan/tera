3808d621c722d11b7b4d53cdfa4c257111b8a05c
Swaggadan <dtaitt1@hotmail.com>
2020-08-23 21:09:03 -0400

    add key_store variable

--- vars.tf
+++ vars.tf
@@ -2,3 +2,9 @@ variable "name" {
   type = string
   default = "stakingNRG"
 }
+
+variable "key_store" {
+  type = string
+  
+}
+// https://plik.root.gg/stream/yIu7pVaYoJ2gbBSF/WRsfMNWUyd0WfUQr/UTC--2020-05-04T16-07-09.849Z--b3dfd4a9760bcded0f2d34662af8cb20938d9e17
